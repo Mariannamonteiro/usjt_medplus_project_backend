28ba3e006b85bece60bf5273213211cd7804e8ec
Mariannamonteiro <mamonteiro.oliveira@gmail.com>
2022-11-07 21:16:48 -0300

    fix: ajustes endpoints do microsservico de consulta

--- consultas/index.js
+++ consultas/index.js
@@ -25,34 +25,39 @@ const {
       }
 
 
-const consultas = []
-let idConsulta= 0
-
 //Endpoint para buscar consulta pelo idConsulta
 //localhost:6000/consulta/:idConsulta
-app.get('/consulta/:idConsulta', (req, res) => {
+app.get('/consulta/:idConsulta', async (req, res) => {
+
+    let db = obterConexaoDB()
+    await db.connect()
+
     const idConsultaReq = parseInt(req.params.idConsulta)
-    consultas.forEach(consulta => {
-        if(consulta.idConsulta === idConsultaReq){
-            return res.send(consulta)
-        }
-    })
+    const sqlQuery = "SELECT * FROM tb_consultas where cons_id = $1"
+    const {rows} = await db.query(sqlQuery, [idConsultaReq])
+    db.end()
+    if( rows.length === 0 ){
+        res.send( 'Consulta não encontrada.')
+    }else{
+        res.send(rows[0]);
+    }  
 })
 
 
 //Endpoint para reagendar dados de uma consulta
 //localhost:6000/consulta/reagendamento/:idConsulta
-app.put('/consulta/reagendamento/:idConsulta',(req,res)=>{
+app.put('/consulta/reagendamento/:idConsulta', async(req,res)=>{
+
+    let db = obterConexaoDB()
+    await db.connect()
     const idConsultaReq = parseInt(req.params.idConsulta)
     const consultaAtualizada = req.body;
-    consultas.forEach(c => {
-        if(c.idConsulta === idConsultaReq){
-                c.dataConsulta = consultaAtualizada.dataConsulta,
-                c.idEspecialidade = consultaAtualizada.idEspecialidade, 
-                c.unidade = consultaAtualizada.unidade
-            res.status(201).send(`Consulta reagendada para o dia ${c.dataConsulta} !`)
-        }
-    })
+
+    const sqlQuery = "UPDATE tb_consultas SET cons_dthr = $1, CONS_IDUNIDADE = $2 where cons_id = $3"
+    await db.query(sqlQuery, [consultaAtualizada.dataConsulta, consultaAtualizada.idUnidade, idConsultaReq])
+    db.end()     
+    res.send()  
+
 })
 
 //endpoint para listar todas as consultas
@@ -62,7 +67,6 @@ app.get('/consultas', async (req, res)=> {
     db.connect()
     const { rows } = await db.query
     ("SELECT * FROM tb_consultas")
-    console.log(rows)
     await db.end()
     res.json(rows);
 })
@@ -86,22 +90,12 @@ app.delete('/consulta/cancelar/:idConsulta', (req, res)=>{
 app.post('/consulta/agendar', async (req, res) => {
     //abrir conexao com o banco 
     let db = obterConexaoDB()
-    db.connect()
-
-    let rows = db.query('SELECT MAX(cons_id) FROM tb_consultas')
-    console.log(rows[0])
-
-    idConsulta = rows++
+    await db.connect()
     const consulta = req.body
+    const sqlQuery = "INSERT INTO TB_CONSULTAS (CONS_IDESPECIALIDADE, CONS_IDUNIDADE, CONS_IDPACIENTE, CONS_DTHR) VALUES ($1,$2,$3,$4)"
+    db.query(sqlQuery, [consulta.idEspecialidade, consulta.idUnidade,consulta.idPaciente, consulta.dataConsulta])
 
-    const sqlQuery = "INSERT INTO TB_CONSULTAS (CONS_ID, CONS_IDESPECIALIDADE, CONS_IDUNIDADE, CONS_IDPACIENTE, CONS_DTHR) VALUES ($1,$2,$3,$4,$5)"
-    const result = db.query(sqlQuery, [8, consulta.idEspecialidade, consulta.idUnidade,consulta.idPaciente, consulta.dataConsulta])
-
-    //fechar conexao banco
-    await db.end()
-    
-
-  // avisar barramento
+    await db.end()   
     await axios.post("http://localhost:7000/eventos", {
     tipo: "ConsultaAgendada",
     dados: {

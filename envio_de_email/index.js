99d32106f337c2630a6a89516d251fe8812860d4
Mariannamonteiro <mamonteiro.oliveira@gmail.com>
2022-11-11 17:42:20 -0300

    fix:ajuste envio de email

--- envio_de_email/index.js
+++ envio_de_email/index.js
@@ -15,7 +15,9 @@ const {
 
 app.post('/envio-de-email', (req, res) => {
 
-    const consulta = req.body.dados.consulta
+    console.log(req.body.consulta)
+    const consulta = req.body.consulta
+
 
     let transporter = nodemailer.createTransport({
         host: EMAIL_HOST,
@@ -27,22 +29,52 @@ app.post('/envio-de-email', (req, res) => {
             pass: EMAIL_PASS
         }
     });
+
+    if(consulta.usuarioLogado === false){
+        console.log("ENTROU NAO ESTA LOGADO")
+        transporter.sendMail({
+            from: `USJT MEDPLUS <${EMAIL_USER}>`,
+            to: consulta.email,
+            subject: "MEDPLUS - Consulta Agendada",
+            html: `Olá ${consulta.nome}, <br> sua consulta foi marcada com sucesso na MEDPLUS, confira: <br>
+            Dia: ${consulta.dataConsulta} <br>
+            Unidade: ${consulta.idUnidade}  <br>
+            <br>
+            Para efetivar se cadastro na MEDPLUS, atualize a senha padrão que criamos nesse link:<a href="https://www.google.com/">Redefina sua senha MEDPLUS</a>.
+            <br>
+            Lembramos que, para consultas em nossas unidades, a apresentação do documento de identificação e da carteirinha do convênio*, são indispensáveis. Bem como o uso de máscara, em função da pandemia da COVID-19
+            `
+        }).then(message =>{
+            console.log(message)
+            res.send()
+        }).catch(err =>{
+            console.log(err)
+        })       
+
+    }else{
+        console.log("ENTROU ESTA LOGADO")
+
+
+        transporter.sendMail({
+            from: `USJT MEDPLUS <${EMAIL_USER}>`,
+            to: consulta.email,
+            subject: "MEDPLUS - Consulta Agendada",
+            html: `Olá ${consulta.nome}, <br> sua consulta foi marcada com sucesso na MEDPLUS, confira: <br>
+            Dia: ${consulta.dataConsulta} <br>
+            Unidade: ${consulta.idUnidade}  <br>
+            Lembramos que, para consultas em nossas unidades, a apresentação do documento de identificação e da carteirinha do convênio*, são indispensáveis. Bem como o uso de máscara, em função da pandemia da COVID-19
+            `
+        }).then(message =>{
+            console.log(message)
+            res.send()
+        }).catch(err =>{
+            console.log(err)
+        })
+
+    }
     
 
-    transporter.sendMail({
-        from: `USJT MEDPLUS <${EMAIL_USER}>`,
-        to: consulta.email,
-        subject: "MEDPLUS - Consulta Agendada",
-        html: `Olá ${consulta.nome}, <br> sua consulta foi marcada com sucesso na MEDPLUS, confira: <br>
-        Dia: ${consulta.dataConsulta} <br>
-        Unidade: ${consulta.unidade}  <br>
-        Lembramos que, para consultas em nossas unidades, a apresentação do documento de identificação e da carteirinha do convênio*, são indispensáveis. Bem como o uso de máscara, em função da pandemia da COVID-19
-        `
-    }).then(message =>{
-        console.log(message)
-    }).catch(err =>{
-        console.log(err)
-    })
+    
     
 
 })

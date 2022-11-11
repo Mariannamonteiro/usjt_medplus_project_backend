6dbb0ba99f702ac3448f55e72e6feae25b44f8a8
Mariannamonteiro <mamonteiro.oliveira@gmail.com>
2022-11-11 17:42:58 -0300

    fix:ajuste endpoints de usuarios

--- usuarios/index.js
+++ usuarios/index.js
@@ -1,116 +1,114 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-app.use(cors())
-app.use(express.json())
+const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
 
-require('dotenv').config()
+require('dotenv').config();
 
-const {
-    USER_DB_USUARIOS,
-    HOST_DB_USUARIOS,
-    DATABASE_DB_USUARIOS,
-    PASSWORD_DB_USUARIOS,
-    PORT_DB_USUARIOS} = process.env
-    const {
-        Client
-      } = require('pg')
-     
-      function obterConexaoDB (){
-        return new Client({
-          user: USER_DB_USUARIOS,
-          host: HOST_DB_USUARIOS,
-          database: DATABASE_DB_USUARIOS,
-          password: PASSWORD_DB_USUARIOS,
-          port: PORT_DB_USUARIOS
-        });
-      }
+const { USER_DB_USUARIOS, HOST_DB_USUARIOS, DATABASE_DB_USUARIOS, PASSWORD_DB_USUARIOS, PORT_DB_USUARIOS } = process.env;
+const { Client } = require('pg');
+
+function obterConexaoDB() {
+  return new Client({
+    user: USER_DB_USUARIOS,
+    host: HOST_DB_USUARIOS,
+    database: DATABASE_DB_USUARIOS,
+    password: PASSWORD_DB_USUARIOS,
+    port: PORT_DB_USUARIOS,
+  });
+}
 
 //endpoint para cadastrar um novo usuario paciente
 //localhost:5000/cadastro/paciente
-app.post('/cadastro/paciente',async (req, res) => {
+app.post('/cadastro/paciente', async (req, res) => {
+  if (req.body.barramento === true) {
+    req.body = req.body.dados;
+  }
+  let db = obterConexaoDB();
+  db.connect();
 
-    if(req.body.barramento === true){
-        req.body = req.body.dados
+  sqlQuerySelect = 'SELECT * FROM tb_pacientes WHERE pac_cpf = $1';
+  const { rows } = await db.query(sqlQuerySelect, [req.body.cpf]);
+
+  if (rows.length > 0) {
+    if (rows[0].cpf === req.body.cpf) {
+      db.end();
+      return res.send('CPF já cadastrado.');
     }
-    let db = obterConexaoDB()
-    db.connect()
-    
-    sqlQuerySelect = "SELECT * FROM tb_pacientes WHERE pac_cpf = $1"
-    const {rows} = await db.query(sqlQuerySelect, [req.body.cpf])
+  } else {
+    paciente = req.body;
+    sqlQueryInsert =
+      'INSERT INTO TB_PACIENTES (PAC_CPF,PAC_NOME,PAC_DTNASCIMENTO,PAC_SEXO,PAC_TELEFONE,PAC_EMAIL,PAC_SENHA)' + 'VALUES($1, $2, $3, $4, $5, $6, $7)';
 
-    if(rows.length > 0){
-        if(paciente.cpf === req.body.cpf){
-            db.end()
-            return res.send("CPF já cadastrado.")
-        }
-    }else{
-        
-        paciente = req.body
-        sqlQueryInsert = "INSERT INTO TB_PACIENTES (PAC_CPF,PAC_NOME,PAC_DTNASCIMENTO,PAC_SEXO,PAC_TELEFONE,PAC_EMAIL,PAC_SENHA)" 
-         + "VALUES($1, $2, $3, $4, $5, $6, $7)"
-        
-        await db.query(sqlQueryInsert, [paciente.cpf, paciente.nome, paciente.dataNascimento, paciente.sexo,paciente.celular, paciente.email, paciente.senha ]) 
+    await db.query(sqlQueryInsert, [
+      paciente.cpf,
+      paciente.nome,
+      paciente.dataNascimento,
+      paciente.sexo,
+      paciente.celular,
+      paciente.email,
+      paciente.senha,
+    ]);
 
-        db.end()
-        res.status(201).send(`Paciente ${paciente.nome} criado com sucesso!`)
+    const { rows } = await db.query(sqlQuerySelect, [req.body.cpf]);
 
-    }
-})
+    db.end();
+    console.log({ usuario: rows[0] })
+    res.json({ usuario: rows[0] }).send()
+    res.status(201).json();
+  }
+});
 
 //endpoint para logar um usuario
 //localhost:5000/login
-app.post('/login', async(req, res) => {
+app.post('/login', async (req, res) => {
+  let db = obterConexaoDB();
+  await db.connect();
 
-    let db = obterConexaoDB()
-    await db.connect()
+  const cpfReq = req.body.cpf;
+  const senhaReq = req.body.senha;
 
-    const cpfReq = req.body.cpf
-    const senhaReq = req.body.senha
-
-    const sqlQuery = "SELECT * FROM tb_pacientes WHERE pac_cpf = $1 AND pac_senha = $2"
-    const {rows} = await db.query(sqlQuery, [cpfReq, senhaReq])
-    usuario = rows[0]
-    if(usuario === undefined){
-        db.end() 
-        res.send("Usuário e/ou senha incorreta.")
-    }else{
-        if (cpfReq === usuario.pac_cpf && senhaReq === usuario.pac_senha) {
-
-            sqlQuerySelect = "SELECT * FROM tb_pacientes WHERE pac_cpf = $1"
-            const {rows} = await db.query(sqlQuerySelect, [cpfReq])
-            usuarioLogado = rows[0]
-            db.end() 
-            return res.send(usuarioLogado)
-        }
-    }    
-    db.end()  
+  const sqlQuery = 'SELECT * FROM tb_pacientes WHERE pac_cpf = $1 AND pac_senha = $2';
+  const { rows } = await db.query(sqlQuery, [cpfReq, senhaReq]);
+  usuario = rows[0];
+  if (usuario === undefined) {
+    db.end();
+    res.send('Usuário e/ou senha incorreta.');
+  } else {
+    if (cpfReq === usuario.pac_cpf && senhaReq === usuario.pac_senha) {
+      sqlQuerySelect = 'SELECT * FROM tb_pacientes WHERE pac_cpf = $1';
+      const { rows } = await db.query(sqlQuerySelect, [cpfReq]);
+      usuarioLogado = rows[0];
+      db.end();
+      return res.send(usuarioLogado);
+    }
+  }
+  db.end();
 });
 
 //Buscar se user existe
 //localhost:5000/buscar/usuario
-app.get('/buscar/usuario', async (req, res)=> {
+app.get('/buscar/usuario/:cpf', async (req, res) => {
+  let db = obterConexaoDB();
+  db.connect();
+  console.log('BODY ORIGINAL');
+  console.log(req.params.cpf);
 
-    let db = obterConexaoDB()
-    db.connect()
-    console.log("BODY ORIGINAL")
-    console.log(req.body)    
+  sqlQuerySelect = 'SELECT * FROM tb_pacientes WHERE pac_cpf = $1';
+  const { rows } = await db.query(sqlQuerySelect, [req.params.cpf]);
+  await db.end();
 
-    sqlQuerySelect = "SELECT * FROM tb_pacientes WHERE pac_cpf = $1";
-    const { rows } = await db.query(sqlQuerySelect, [req.body.cpfUser])
-    await db.end()
+  console.log(rows[0]);
+  if(rows.length > 0){
+    console.log("ENCONTROU USER")
+    res.json({usuario: rows[0]}).send();
+  }else{
+    console.log("NÃO ENCONTROU USER")
+    res.json({usuario: null}).send();
+  }
+});
 
-    console.log("CPF REQ::" + req.cpfUser)
-    console.log("ESTA RETORNANDO?")
-    console.log(rows[0])
-    res.json({resultado: rows.length === 0 ? [] : rows[0]});
-})
-
-
-
-
-
-
-app.listen(5000, () => { console.log("Usuários. Porta 5000") })
-
-
+app.listen(5000, () => {
+  console.log('Usuários. Porta 5000');
+});
